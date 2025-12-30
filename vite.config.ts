3abac8e59b21b276b03ce22fd2e5c395708e99fb
Matthew Wesney <157447210+dovvnloading@users.noreply.github.com>
2025-12-30 14:09:09 -0500

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // CRITICAL CHANGE: This must match your repository name exactly, surrounded by slashes.
-  base: '/Graphite-Git/', 
-})
